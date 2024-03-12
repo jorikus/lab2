8969c143a01c16862ec309e9cef706d8c65b2ec4
jorikus <voroschilow.eg@yandex.ru>
2024-03-12 20:19:47 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,9 @@
 import pytest
 from function import subtract_two_numbers
 
+def test_subtract_two_numbers():
+    assert subtract_two_numbers(4, 2) == 2
+    assert subtract_two_numbers(1, 3) == -2
+    assert subtract_two_numbers(4.2 - 2.1) == 2.1
+
 
